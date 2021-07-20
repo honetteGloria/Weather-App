4c24f5fc2c98bc571d05eef458e8822768fa59e3
Honette Gloria Benihirwe <benihgloria@gmail.com>
2021-07-20 00:55:19 -0400

    Update app.js

--- app.js
+++ app.js
@@ -13,6 +13,14 @@ window.addEventListener('load', ()=>{
             lat = position.coords.latitude;
             //get the weather information from tomorrow API-- it's free 
             //designed for developers and relied on by successfull companies like Uber, ws, Google Cloud
+            
+            /* use a CORS(Cross Orgin Resource sharing) proxy
+            requests that do not come from the same origin as the hosted data will be rejected, protecting the origin source from a variety 
+            of complex attacks including cross-domain scripts, redirects, and more. While this is designed to protect the end-user, 
+            this proxy serves as a middleman transformative layer, allowing CORS issues to be bypassed entirely.
+            it can also be a significant development blocker
+            */
+            
             const api = `curl --request GET --url \
             'https://api.tomorrow.io/v4/timelines?location=${lat},${long}&fields=temperature&timesteps=1h&units=metric&apikey=a61O9G4FDaGWUea6RJtvGMQcf9sOYgwS'`;
 
